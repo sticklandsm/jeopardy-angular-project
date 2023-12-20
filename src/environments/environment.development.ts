3a3f980838027e88885dc1671dd079b5a9790b3d
Sean Stickland <sticklandsm@gmail.com>
2023-12-20 13:22:00 +1300

    dsfsdf s

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8999/',
-  wsUrl: 'ws://localhost:8999/',
+  apiUrl: 'https://jeopardy-backend.azurewebsites.net/',
+  wsUrl: 'wss://jeopardy-backend.azurewebsites.net/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://jeopardy-backend.azurewebsites.net',
-  wsUrl: 'wss://jeopardy-backend.azurewebsites.net',
+  apiUrl: 'https://jeopardy-backend.azurewebsites.net/',
+  wsUrl: 'wss://jeopardy-backend.azurewebsites.net/',
 };
