90b874638b7cf38dc3e42d57bdeae866f1d43e8b
Sean Stickland <sticklandsm@gmail.com>
2024-01-15 14:08:58 +1300

    editted how to play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
 
   onClickShowHowToPlay({ target }: MouseEvent) {
     alert(
-      'Welcome to Jeopardy! In this game you are given several categories of trivia to choose from each with dollar ammounts. Click an amount and you will be given a Clue. If you think you know the response to this clue, click it again and type in your response. You will be given or taken away money depending on if your response was correct. There is a timer though so answer quickly. If no one responds in time, you should hear a beeping noise and you can go onto the next clue'
+      'Welcome to Jeopardy! In this multi player game you are given several categories of trivia to choose from each with dollar ammounts. Click an amount and you will be given a Clue. If you think you know the response to this clue, click it again and type in your response. You will be given or taken away money depending on if your response was correct. There is a timer though so answer quickly. If no one responds in time, you should hear a beeping noise and you can go onto the next clue. This game can be played with other people, just send them the link to your game.'
     );
     return;
   }
